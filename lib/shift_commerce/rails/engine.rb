791a1008968af0d29a05134c1ca89fc19431a2a2
Kris Quigley <kris@krisquigley.co.uk>
2016-03-31 13:22:35 +0100

    Removed eager loading

--- lib/shift_commerce/rails/engine.rb
+++ lib/shift_commerce/rails/engine.rb
@@ -3,15 +3,6 @@ module ShiftCommerce
     class Engine < ::Rails::Engine
       isolate_namespace ShiftCommerce::Rails
 
-      def eager_load!
-        config.eager_load_paths.each do |load_path|
-          matcher = /\A#{Regexp.escape(load_path.to_s)}\/(.*)\.rb\Z/
-          Dir.glob("#{load_path}/**/*.rb").sort.each do |file|
-            require_dependency file.sub(matcher, '\1')
-          end
-        end
-      end
-
       initializer "shift_commerce-rails.set_helpers_path", before: "action_controller.set_helpers_path" do |app|
         app.config.helpers_paths.unshift File.expand_path("../../../app/helpers", __dir__)
       end
